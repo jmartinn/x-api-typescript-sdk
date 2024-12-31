fd74463d20692b5180a573703c1e63393db9f72d
Juan Pedro Martin <juamp_m@hotmail.com>
2024-12-31 01:30:55 +0100

    refactor: update examples for improved usability and correctness

--- examples/oauth2-bearer.ts
+++ examples/oauth2-bearer.ts
@@ -1,14 +1,15 @@
 // Copyright 2021 Twitter, Inc.
 // SPDX-License-Identifier: Apache-2.0
+// Modifications Copyright 2024 Juan Pedro Martin
 
-import { Client } from "twitter-api-sdk";
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
+import { Client } from 'x-api-sdk';
 
 dotenv.config();
 
 async function main() {
   const client = new Client(process.env.BEARER_TOKEN as string);
-  const { data } = await client.users.findUserByUsername("TwitterDev");
+  const { data } = await client.users.findUserByUsername('TwitterDev');
   if (!data) throw new Error("Couldn't find user");
   let count = 0;
   for await (const followers of client.users.usersIdFollowers(data.id)) {
--- examples/oauth2-callback.ts
+++ examples/oauth2-callback.ts
@@ -1,9 +1,10 @@
 // Copyright 2021 Twitter, Inc.
 // SPDX-License-Identifier: Apache-2.0
+// Modifications Copyright 2024 Juan Pedro Martin
 
-import { Client, auth } from "twitter-api-sdk";
-import express from "express";
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import express from 'express';
+import { Client, auth } from 'x-api-sdk';
 
 dotenv.config();
 
@@ -12,39 +13,39 @@ const app = express();
 const authClient = new auth.OAuth2User({
   client_id: process.env.CLIENT_ID as string,
   client_secret: process.env.CLIENT_SECRET as string,
-  callback: "http://127.0.0.1:3000/callback",
-  scopes: ["tweet.read", "users.read"],
+  callback: 'http://127.0.0.1:3000/callback',
+  scopes: ['tweet.read', 'users.read'],
 });
 
 const client = new Client(authClient);
 
-const STATE = "my-state";
+const STATE = 'my-state';
 
-app.get("/callback", async function (req, res) {
+app.get('/callback', async function (req, res) {
   try {
     const { code, state } = req.query;
     if (state !== STATE) return res.status(500).send("State isn't matching");
     await authClient.requestAccessToken(code as string);
-    res.redirect("/tweets");
+    res.redirect('/tweets');
   } catch (error) {
     console.log(error);
   }
 });
 
-app.get("/login", async function (req, res) {
+app.get('/login', async function (req, res) {
   const authUrl = authClient.generateAuthURL({
     state: STATE,
-    code_challenge_method: "s256",
+    code_challenge_method: 's256',
   });
   res.redirect(authUrl);
 });
 
-app.get("/tweets", async function (req, res) {
-  const tweets = await client.tweets.findTweetById("20");
+app.get('/tweets', async function (req, res) {
+  const tweets = await client.tweets.findTweetById('20');
   res.send(tweets.data);
 });
 
-app.get("/revoke", async function (req, res) {
+app.get('/revoke', async function (req, res) {
   try {
     const response = await authClient.revokeAccessToken();
     res.send(response);
@@ -52,8 +53,6 @@ app.get("/revoke", async function (req, res) {
     console.log(error);
   }
 });
-
-
 
 app.listen(3000, () => {
   console.log(`Go here to login: http://127.0.0.1:3000/login`);
